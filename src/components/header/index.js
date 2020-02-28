eaa11b9daaf96c9ddd5fdbd2b2ab912af888b4ad
Vinicius Strauss <vstrauss@ac-vstrauss.lan>
2020-02-27 21:50:52 -0300

    put my profile image

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+
 import "./styles.css";
 
 class HeaderComponent extends Component {
@@ -7,6 +10,9 @@ class HeaderComponent extends Component {
     return (
       <div className="header">
         <div className="content">facebook</div>
+        <div className="profile">
+          Meu perfil <FontAwesomeIcon icon={faUserCircle}></FontAwesomeIcon>
+        </div>
       </div>
     );
   }
